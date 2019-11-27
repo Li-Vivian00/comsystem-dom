f629b43eb003583df41d57867094bd51a1c770cf
kinglww <370578047@qq.com>
2019-11-27 19:00:27 +0800

    change user_id to loginid

--- src/main/java/com/example/comsystem/repository/user/RoomInfoRepository.java
+++ src/main/java/com/example/comsystem/repository/user/RoomInfoRepository.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 
 public interface RoomInfoRepository extends JpaRepository<RoomInfo, String> {
-    @Query(value = "select count(user_id) from room_info where user_id is not null and room_id like CONCAT('%',:a1,'%')", nativeQuery = true)
+    @Query(value = "select count(loginid) from room_info where loginid is not null and room_id like CONCAT('%',:a1,'%')", nativeQuery = true)
     Integer getCountOfUser(@Param("a1") String a1);
 
-    @Query(value = "select AVG(score) from room_info where user_id is not null and room_id like CONCAT('%',:a1,'%')", nativeQuery = true)
+    @Query(value = "select AVG(score) from room_info where loginid is not null and room_id like CONCAT('%',:a1,'%')", nativeQuery = true)
     Double getAvgScore(String a1);
 
     @Query(value = "select * from room_info where room_id LIKE CONCAT('%',:floorId,'%')", nativeQuery = true)
